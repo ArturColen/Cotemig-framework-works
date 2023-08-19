0637156f76d1e12d2aa80839e4a94ad51f2092cf
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-08-18 22:40:29 -0300

    refactor: improving GitHub repositories API request

--- 2nd stage/08-react-portfolio/src/pages/Projects/index.jsx
+++ 2nd stage/08-react-portfolio/src/pages/Projects/index.jsx
@@ -7,31 +7,43 @@ function Projects() {
 
     useEffect(() => {
         const fetchRepositories = async () => {
-            const response = await fetch('https://api.github.com/users/ArturColen/repos');
-            const data = await response.json();
-            setRepositories(data);
+            try {
+                const response = await fetch('https://api.github.com/users/ArturColen/repos');
+
+                if (!response.ok) {
+                    throw new Error('Falha ao buscar dados');
+                }
+
+                const data = await response.json();
+                setRepositories(data);
+            }
+            catch (error) {
+                console.error('Erro ao buscar dados:', error);
+            }
         }
 
         fetchRepositories();
     }, []);
 
     return (
-        <section className={styles.projects}>
-            <h2>Projetos</h2>
-            {
-                repositories.length > 0 ? (
-                    <section className={styles.list}>
-                        {
-                            repositories.map((repo) => (
-                                <Card key={repo.id} name={repo.name} description={repo.description} html_url={repo.html_url} />
-                            ))
-                        }
-                    </section>
-                ) : (
-                    <p>Carregando repositórios...</p>
-                )
-            }
-        </section>
+        <>
+            <section className={styles.projects}>
+                <h2>Projetos</h2>
+                {
+                    repositories.length > 0 ? (
+                        <section className={styles.list}>
+                            {
+                                repositories.map((repo) => (
+                                    <Card key={repo.id} name={repo.name} description={repo.description} html_url={repo.html_url} />
+                                ))
+                            }
+                        </section>
+                    ) : (
+                        <p>Carregando repositórios...</p>
+                    )
+                }
+            </section>
+        </>
     );
 }
 
